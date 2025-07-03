e4fd6081588aa477b2b05deaeaf52db2c368c016
Stefan Z <100451534+Zauni1984@users.noreply.github.com>
2025-07-03 02:50:16 +0200

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -8,16 +8,14 @@
 
 #include <uint256.h>
 #include <limits>
-#include <map>
-#include <string>
 
 namespace Consensus {
 
 enum DeploymentPos
 {
     DEPLOYMENT_TESTDUMMY,
-    DEPLOYMENT_CSV, // Deployment of BIP68, BIP112, and BIP113.
-    DEPLOYMENT_SEGWIT, // Deployment of BIP141, BIP143, and BIP147.
+    DEPLOYMENT_CSV,     // Deployment of BIP68, BIP112, and BIP113.
+    DEPLOYMENT_SEGWIT,  // Deployment of BIP141, BIP143, and BIP147.
     // NOTE: Also add new deployments to VersionBitsDeploymentInfo in versionbits.cpp
     MAX_VERSION_BITS_DEPLOYMENTS
 };
@@ -37,9 +35,8 @@ struct BIP9Deployment {
     static constexpr int64_t NO_TIMEOUT = std::numeric_limits<int64_t>::max();
 
     /** Special value for nStartTime indicating that the deployment is always active.
-     *  This is useful for testing, as it means tests don't need to deal with the activation
-     *  process (which takes at least 3 BIP9 intervals). Only tests that specifically test the
-     *  behaviour during activation cannot use this. */
+     *  Useful for tests that should bypass activation mechanics.
+     */
     static constexpr int64_t ALWAYS_ACTIVE = -1;
 };
 
@@ -49,33 +46,46 @@ struct BIP9Deployment {
 struct Params {
     uint256 hashGenesisBlock;
     int nSubsidyHalvingInterval;
+
     /** Block height at which BIP16 becomes active */
     int BIP16Height;
+
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
     uint256 BIP34Hash;
+
     /** Block height at which BIP65 becomes active */
     int BIP65Height;
+
     /** Block height at which BIP66 becomes active */
     int BIP66Height;
+
     /**
-     * Minimum blocks including miner confirmation of the total of 2016 blocks in a retargeting period,
-     * (nPowTargetTimespan / nPowTargetSpacing) which is also used for BIP9 deployments.
-     * Examples: 1916 for 95%, 1512 for testchains.
+     * Minimum number of blocks including miner confirmation of the total in a retargeting period,
+     * (nPowTargetTimespan / nPowTargetSpacing), also used for BIP9 deployments.
+     * Examples: 1916 for 95%, 1512 for testnets.
      */
     uint32_t nRuleChangeActivationThreshold;
     uint32_t nMinerConfirmationWindow;
+
+    /** BIP9 deployments */
     BIP9Deployment vDeployments[MAX_VERSION_BITS_DEPLOYMENTS];
-    /** Proof of work parameters */
+
+    /** Proof of Work parameters */
     uint256 powLimit;
     bool fPowAllowMinDifficultyBlocks;
     bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
-    int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
+
+    int64_t DifficultyAdjustmentInterval() const {
+        return nPowTargetTimespan / nPowTargetSpacing;
+    }
+
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
-    /* DigiShield params */
+
+    /** DigiShield parameters */
     int64_t nAveragingInterval;
     int64_t nMaxAdjustDownV4;
     int64_t nMaxAdjustUpV4;
@@ -84,9 +94,11 @@ struct Params {
     int64_t nMaxActualTimespanV4;
     int64_t nLocalTargetAdjustment;
     int nMinActualTimespanV4;
-    /*End DigiSheild params*/
-    int nDigiSheildHFHeight;
+
+    /** Block height at which DigiShield becomes active */
+    int nDigiShieldHFHeight;
 };
+
 } // namespace Consensus
 
 #endif // BITCOIN_CONSENSUS_PARAMS_H
