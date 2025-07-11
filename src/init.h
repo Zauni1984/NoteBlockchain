800575c3e76e3097599900c613ad636dd1c6d131
Stefan Z <100451534+Zauni1984@users.noreply.github.com>
2025-07-12 00:05:14 +0200

    Update init.h
    Prepared 12 word seed phrase

--- src/init.h
+++ src/init.h
@@ -1,70 +1,48 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2017 The Bitcoin Core developers
+// Copyright (c) 2025 NoteCoin Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_INIT_H
-#define BITCOIN_INIT_H
+#ifndef NOTECOIN_INIT_H
+#define NOTECOIN_INIT_H
 
 #include <string>
 
 class CScheduler;
-class CWallet;
 
-namespace boost
-{
+namespace boost {
 class thread_group;
 } // namespace boost
 
+// Shutdown-Steuerung
 void StartShutdown();
 bool ShutdownRequested();
-/** Interrupt threads */
 void Interrupt();
 void Shutdown();
-//!Initialize the logging infrastructure
-void InitLogging();
-//!Parameter interaction: change current parameters depending on various rules
-void InitParameterInteraction();
 
-/** Initialize bitcoin core: Basic context setup.
- *  @note This can be done before daemonization. Do not call Shutdown() if this function fails.
- *  @pre Parameters should be parsed and config file should be read.
- */
-bool AppInitBasicSetup();
-/**
- * Initialization: parameter interaction.
- * @note This can be done before daemonization. Do not call Shutdown() if this function fails.
- * @pre Parameters should be parsed and config file should be read, AppInitBasicSetup should have been called.
- */
-bool AppInitParameterInteraction();
-/**
- * Initialization sanity checks: ecc init, sanity checks, dir lock.
- * @note This can be done before daemonization. Do not call Shutdown() if this function fails.
- * @pre Parameters should be parsed and config file should be read, AppInitParameterInteraction should have been called.
- */
-bool AppInitSanityChecks();
-/**
- * Lock bitcoin core data directory.
- * @note This should only be done after daemonization. Do not call Shutdown() if this function fails.
- * @pre Parameters should be parsed and config file should be read, AppInitSanityChecks should have been called.
- */
-bool AppInitLockDataDirectory();
-/**
- * Bitcoin core main initialization.
- * @note This should only be done after daemonization. Call Shutdown() if this function fails.
- * @pre Parameters should be parsed and config file should be read, AppInitLockDataDirectory should have been called.
- */
-bool AppInitMain();
+// Logging & Parameterhandling
+void InitLogging();                   // Logging-Infrastruktur initialisieren
+void InitParameterInteraction();      // Interaktionen und Abhängigkeiten von Parametern prüfen
 
-/** The help message mode determines what help message to show */
+// Setup-Phasen der Anwendung
+bool AppInitBasicSetup();             // Initialer System-Setup (z. B. Umgebungsprüfung)
+bool AppInitParameterInteraction();  // Abhängigkeiten von Konfigurationsparametern
+bool AppInitSanityChecks();          // Sanity Checks wie ECC, Datei-Zugriffe usw.
+bool AppInitLockDataDirectory();     // Sperren des Datenverzeichnisses
+bool AppInitMain();                  // Hauptinitialisierung nach Daemonisierung
+
+// SeedPhrase-Manager (für zukünftige Wallet-Wiederherstellung)
+bool InitSeedManager();              // Initialisiert das Seed-Verwaltungssystem
+void ShutdownSeedManager();          // Beendet das Seed-System kontrolliert
+
+// Hilfe & Lizenzinformationen
 enum HelpMessageMode {
     HMM_BITCOIND,
     HMM_BITCOIN_QT
 };
 
-/** Help for options shared between UI and daemon (for -help) */
 std::string HelpMessage(HelpMessageMode mode);
-/** Returns licensing information (for -version) */
 std::string LicenseInfo();
 
-#endif // BITCOIN_INIT_H
+#endif // NOTECOIN_INIT_H
