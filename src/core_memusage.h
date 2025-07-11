ab06ab75d260f1821e6cf2ace7ec52f3876a67fa
Stefan Z <100451534+Zauni1984@users.noreply.github.com>
2025-07-11 23:33:56 +0200

    Update core_memusage.h

--- src/core_memusage.h
+++ src/core_memusage.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-2017 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Distributed under the MIT software license.
 
 #ifndef BITCOIN_CORE_MEMUSAGE_H
 #define BITCOIN_CORE_MEMUSAGE_H
@@ -9,59 +8,109 @@
 #include <primitives/block.h>
 #include <memusage.h>
 
+/**
+ * Diese Datei definiert rekursive Speicherverbrauchsberechnungen
+ * für Transaktions- und Blockobjekte zur genaueren Analyse
+ * des dynamisch allokierten Speichers.
+ */
+
+// ---------------------------------
+// Einzelobjekte
+// ---------------------------------
+
 static inline size_t RecursiveDynamicUsage(const CScript& script) {
     return memusage::DynamicUsage(script);
 }
 
 static inline size_t RecursiveDynamicUsage(const COutPoint& out) {
-    return 0;
+    return 0; // Enthält keine dynamischen Daten
 }
 
+// ---------------------------------
+// Transaktionseingänge
+// ---------------------------------
+
 static inline size_t RecursiveDynamicUsage(const CTxIn& in) {
-    size_t mem = RecursiveDynamicUsage(in.scriptSig) + RecursiveDynamicUsage(in.prevout) + memusage::DynamicUsage(in.scriptWitness.stack);
-    for (std::vector<std::vector<unsigned char> >::const_iterator it = in.scriptWitness.stack.begin(); it != in.scriptWitness.stack.end(); it++) {
-         mem += memusage::DynamicUsage(*it);
+    size_t mem = 0;
+    mem += RecursiveDynamicUsage(in.scriptSig);
+    mem += RecursiveDynamicUsage(in.prevout);
+    mem += memusage::DynamicUsage(in.scriptWitness.stack);
+
+    for (const auto& element : in.scriptWitness.stack) {
+        mem += memusage::DynamicUsage(element);
     }
+
     return mem;
 }
+
+// ---------------------------------
+// Transaktionsausgänge
+// ---------------------------------
 
 static inline size_t RecursiveDynamicUsage(const CTxOut& out) {
     return RecursiveDynamicUsage(out.scriptPubKey);
 }
 
+// ---------------------------------
+// Transaktionen
+// ---------------------------------
+
 static inline size_t RecursiveDynamicUsage(const CTransaction& tx) {
-    size_t mem = memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout);
-    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin(); it != tx.vin.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+    size_t mem = 0;
+    mem += memusage::DynamicUsage(tx.vin);
+    mem += memusage::DynamicUsage(tx.vout);
+
+    for (const auto& in : tx.vin) {
+        mem += RecursiveDynamicUsage(in);
     }
-    for (std::vector<CTxOut>::const_iterator it = tx.vout.begin(); it != tx.vout.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+    for (const auto& out : tx.vout) {
+        mem += RecursiveDynamicUsage(out);
     }
+
     return mem;
 }
 
 static inline size_t RecursiveDynamicUsage(const CMutableTransaction& tx) {
-    size_t mem = memusage::DynamicUsage(tx.vin) + memusage::DynamicUsage(tx.vout);
-    for (std::vector<CTxIn>::const_iterator it = tx.vin.begin(); it != tx.vin.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+    size_t mem = 0;
+    mem += memusage::DynamicUsage(tx.vin);
+    mem += memusage::DynamicUsage(tx.vout);
+
+    for (const auto& in : tx.vin) {
+        mem += RecursiveDynamicUsage(in);
     }
-    for (std::vector<CTxOut>::const_iterator it = tx.vout.begin(); it != tx.vout.end(); it++) {
-        mem += RecursiveDynamicUsage(*it);
+    for (const auto& out : tx.vout) {
+        mem += RecursiveDynamicUsage(out);
     }
+
     return mem;
 }
 
+// ---------------------------------
+// Blöcke
+// ---------------------------------
+
 static inline size_t RecursiveDynamicUsage(const CBlock& block) {
     size_t mem = memusage::DynamicUsage(block.vtx);
+
     for (const auto& tx : block.vtx) {
-        mem += memusage::DynamicUsage(tx) + RecursiveDynamicUsage(*tx);
+        mem += memusage::DynamicUsage(tx);
+        mem += RecursiveDynamicUsage(*tx);
     }
+
     return mem;
 }
+
+// ---------------------------------
+// Block-Locator (für Sync-Mechanismen)
+// ---------------------------------
 
 static inline size_t RecursiveDynamicUsage(const CBlockLocator& locator) {
     return memusage::DynamicUsage(locator.vHave);
 }
+
+// ---------------------------------
+// Shared Pointers auf Objekte
+// ---------------------------------
 
 template<typename X>
 static inline size_t RecursiveDynamicUsage(const std::shared_ptr<X>& p) {
